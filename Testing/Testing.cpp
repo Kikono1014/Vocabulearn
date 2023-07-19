d69bde6a1edc5d4248cc77f62055846821f0129d
Kikono1014 <vitalik.pyvovarchuk@gmail.com>
2023-07-19 09:53:15 +0300

    change sequencees in main inputs and testing methods declaration

--- Testing/Testing.cpp
+++ Testing/Testing.cpp
@@ -5,6 +5,13 @@ Testing::Testing()
 
 }
 
+void Testing::runTest (json dict, int type)
+{
+    vector<int> test { createTest(dict, type) };
+    map<int, bool> results { doTest(dict, test) };
+    showResults(dict, results);
+}
+
 vector<int> Testing::createTest (json dict, int type)
 {
 
@@ -20,13 +27,6 @@ void Testing::showResults (json dict, map<int, bool> results)
 
 }
 
-
-void Testing::runTest (json dict, int type)
-{
-    vector<int> test { createTest(dict, type) };
-    map<int, bool> results { doTest(dict, test) };
-    showResults(dict, results);
-}
 
 Testing::~Testing()
 {
